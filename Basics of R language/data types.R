2d9b98b1cb8a6e546779ad6ab99f894951b43374
akkinasrikar <akkinasrikar@gmail.com>
2021-03-18 12:05:32 +0530

    R

---
+++ Basics of R language/data types.R
@@ -0,0 +1,19 @@
+print(TRUE)
+print(FALSE)
+a <- T
+b <- F
+print(a)
+print(b)
+
+c <- 'hello'
+d <- "hello"
+
+print(c)
+print(d)
+print(class(a))
+print(class(c))
+print(class(10))
+print((class(2.235)))
+
+v <- c(1,2,3)
+print(class(v))
