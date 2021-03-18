ef1e92e1fc3173eca07ce041038195ac339a68b7
akkinasrikar <akkinasrikar@gmail.com>
2021-03-18 12:30:20 +0530

    R

---
+++ Basics of R language/comparison operators.R
@@ -0,0 +1,20 @@
+print(2<4)
+print(3==3)
+print(10==100)
+print(4>20)
+print(1!=10)
+
+v <- 10
+print(v)
+print(v < -20)
+v <-1
+print(v)
+
+v1 <- c(1,2,3)
+v2 <- c(1,3,2)
+
+print(v1<v2)
+print(v1==v2)
+
+print(v1 < 2)
+print(v1<=v2)
