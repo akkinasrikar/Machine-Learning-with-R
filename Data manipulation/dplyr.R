897ad4e7e9b857a0ec6b2db95918786b20b63e45
akkinasrikar <akkinasrikar@gmail.com>
2021-03-28 14:11:48 +0530

    R

--- Data manipulation/dplyr.R
+++ Data manipulation/dplyr.R
@@ -16,5 +16,27 @@ print(slice(flights,1:10))
 print(head(arrange(flights,year,month,day,arr_time)))
 print(head(arrange(flights,year,month,day,desc(arr_time))))
 
+#select
+print(head(select(flights,carrier)))
+print(head(select(flights,carrier,arr_time)))
+
+#rename
+print(head(rename(flights,airline_carrier=carrier)))
+
+#distinct
+print(distinct(select(flights,carrier)))
+
+#mutate 
+print(head(mutate(flights,new_col=arr_delay-dep_delay))) #return entire df
+print(head(transmute(flights,new_col=arr_delay-dep_delay))) #return newly created column
+
+print(summarise(flights,avg_air_time=mean(air_time,na.rm=T)))
+print(summarise(flights,total_air_time=sum(air_time,na.rm=T)))
+
+#get sample n random rows
+print(sample_n(flights,10))
+#in fractions
+print(sample_n(flights,0.8)) #30%
+
 
 
